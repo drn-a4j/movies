26c5e61185b3be4a6c9b3189d03459101be880e2
Darkhan <drn.a4j@gmail.com>
2021-02-14 18:10:39 +0600

    HW01

--- app/src/main/java/ru/mikhailskiy/intensiv/data/MockRepository.kt
+++ app/src/main/java/ru/mikhailskiy/intensiv/data/MockRepository.kt
@@ -16,4 +16,17 @@ object MockRepository {
         return moviesList
     }
 
+    fun getTvShows(): List<TvShow> {
+        val tvShowsList = mutableListOf<TvShow>()
+        for (x in 0..10) {
+            val tvShow = TvShow(
+                title = "Spider-Man $x",
+                voteAverage = 10.0 - x
+            )
+            tvShowsList.add(tvShow)
+        }
+
+        return tvShowsList
+    }
+
 }
